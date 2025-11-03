7cb2b8a7ed344ad1575f33da71a14410961c2a2a
Shradha Garg <shradhagarg0909@gmail.com>
2025-11-03 19:32:13 +0530

    Task1 (Welcome Message App)

---
+++ EXPERIMENTS/EXPERIMENT_5/script.js
@@ -0,0 +1,6 @@
+// Pop up message shows like alert box
+let userName = prompt("What is your name?");
+
+alert("Welcome, " + userName + "!");
+// the message on the webpage using document.write().
+document.write("<h2>Hello " + userName + "! Welcome here.</h2>");
